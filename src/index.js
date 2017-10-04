ada2a3a33abd754270dabf026bc3bda258ae279b
SahilThakur7 <sahilrd07@gmail.com>
2017-10-04 15:51:20 +0530

    Started with the full fledged app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-// Create a new componenet. This compoenet should produce some HTML.
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyDYgyXHG7AlrC1EYzM2PozXkWCoJ48jiA8';
+// Create a new component. This component should produce some HTML.
 const App = function() {
     return <div>Bunty Baweja!</div>;
 } 
 
 // Take this component's generated HTML and put it on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
