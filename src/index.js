2043388e43be9601712232f19a321871935a96cc
SahilThakur7 <sahilrd07@gmail.com>
2017-10-05 12:40:48 +0530

    Class Components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDYgyXHG7AlrC1EYzM2PozXkWCoJ48jiA8';
 // Create a new component. This component should produce some HTML.
 const App = function() {
-    return <div>Bunty Baweja!</div>;
+    return(
+        <div>
+            <SearchBar />
+        </div>
+    )
 } 
 
 // Take this component's generated HTML and put it on the page (in the DOM)
