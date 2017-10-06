af45e6f30d5a20399bf604b20406db80903e653d
SahilThakur7 <sahilrd07@gmail.com>
2017-10-06 15:52:48 +0530

    starting controlled elements

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = { term: ''};
+    }
     render(){
-        return <input />;
-
+        return ( 
+            <div>
+            <input onChange={(event) => this.setState({term: event.target.value})} />
+            Value of the input: {this.state.term}
+            </div>
+    )
     }
 }
+
+
 
 export default SearchBar;
