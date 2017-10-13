9bed88b9f7db8221ee38986119251aec1dbf7773
SahilThakur7 <sahilrd07@gmail.com>
2017-10-13 10:33:53 +0530

    added throttling

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+import _ from 'lodash';
 
 const API_KEY = 'AIzaSyDYgyXHG7AlrC1EYzM2PozXkWCoJ48jiA8';
 
@@ -29,9 +30,10 @@ class App extends React.Component{
 
     }
     render(){
+        const videoSearch = _.debounce((term) => {this.videoSearch(term), 300});
         return(
             <div>
-                <SearchBar onSearchTermChange={((term) => {this.videoSearch(term)})} />
+                <SearchBar onSearchTermChange={videoSearch} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                     onVideoSelect = { (selectedVideo) => {this.setState({selectedVideo})}}
